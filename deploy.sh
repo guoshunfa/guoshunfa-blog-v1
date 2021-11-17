9775d80479cd62fff496f68bc0bdd31612e3a922
郭顺发 <13301203454@163.com>
2021-11-17 09:36:18 +0800

    20211117

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+# set -e
 
 # 生成静态文件
-yarn build
+# yarn build
 
 # 进入生成的文件夹
-cd ./docs/.vuepress/dist
+# cd ./docs/.vuepress/dist
 
-echo doc.pandacode.cn > CNAME
+# echo doc.pandacode.cn > CNAME
 
-# git
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:guoshunfa/pandacode-doc master:blog-dist
+# # git
+# git init
+# git add -A
+# git commit -m 'deploy'
+# git push -f https://guoshunfa:guoshunfa990709@gitee.com/guoshunfa/panda-doc.git master:home
 
-cd -
+# 直接copy到服务器上
+#!/usr/local/bin/expect -f
+# scp -r ./docs/.vuepress/dist/* root@49.232.167.211:/www/wwwroot/doc.pandacode.cn
+spawn scp -r ./docs/.vuepress/dist/* root@49.232.167.211:/www/wwwroot/doc.pandacode.cn
+expect Guoshunfa990709
+# cd -
