34f0328d8c0f47b3877bd2045b55359b8a9c9f8c
郭顺发 <13301203454@163.com>
2021-12-15 15:49:18 +0800

    支持打包后本地查看静态文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
+  base: "./",
   // theme: 'vdoing', // 使用npm包主题
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
