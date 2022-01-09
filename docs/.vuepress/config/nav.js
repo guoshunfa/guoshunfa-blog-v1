99f7db12dabe1f24b359ca466575a8817ee02a26
郭顺发 <13301203454@163.com>
2022-01-09 11:12:13 +0800

    调整菜单

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,175 +5,23 @@ module.exports = [
     link: "/",
   },
   {
-    text: "🌹 Java",
+    text: "🌹 开发",
     items: [
       {
-        text: "Java 基础",
+        text: "Java",
         items: [
           {
-            text: "Java基础 - 面向对象",
+            text: "Java 基础",
             link: "/pages/380156/"
           },
           {
-            text: "Java基础 - 知识点",
-            link: "/pages/50aa3d/"
-          },
-        ]
-      },
-      {
-        text: "Java 集合框架",
-        items: [
-          {
-            text: "Collection - ArrayList 源码解读",
-            link: "/pages/44875a/"
-          },
-        ]
-      },
-      {
-        text: "Java 多线程与并发",
-        items: [
-          {
-            text: "Java 并发知识体系",
-            link: "/pages/288418/"
-          },
-          {
-            text: "Java 并发 - 并发理论",
-            link: "/pages/e112f0/"
-          },
-          {
-            text: "Java 并发 - 线程理论",
-            link: "/pages/1b07e4/"
-          },
-        ]
-      },
-    ]
-  },
-  {
-    text: "Java 相关框架",
-    items: [
-      {
-        text: "Spring",
-        items: [
-          {
-            text: "Spring Event",
+            text: "Java 相关框架",
             link: "/pages/8eecf9/"
           },
-          {
-            text: "Spring 事务",
-            link: "/pages/6d5a00/"
-          },
         ]
       },
       {
-        text: "SpringBoot 入门",
-        items: [
-          {
-            text: "SpringBoot - 基础知识",
-            link: "/pages/d30647/"
-          },
-        ]
-      },
-      {
-        text: "Maven",
-        items: [
-          {
-            text: "Maven setting 详解",
-            link: "/pages/65f1e8/"
-          },
-          {
-            text: "Nexus maven 私服配置",
-            link: "/pages/c03191/"
-          },
-        ]
-      },
-    ]
-  },
-  {
-    text: "数据库",
-    items: [
-      {
-        text: "SQL 数据库",
-        items: [
-          {
-            text: "Mysql",
-            link: "/pages/d51cca/",
-          },
-        ],
-      },
-      {
-        text: "NOSQL 数据库",
-        items: [
-          {
-            text: "MongoDB",
-            link: "/pages/b58ac5/",
-          },
-          {
-            text: "Redis",
-            link: "/pages/3ae5e3/",
-          },
-        ],
-      },
-    ]
-  },
-  {
-    text: "部署",
-    items: [
-      {
-        text: "服务器",
-        items: [
-          {
-            text: "树莓派 资料记录",
-            link: "/pages/fc21a3/",
-          },
-        ]
-      },
-      {
-        text: "Docker",
-        items: [
-          {
-            text: "Docker 介绍",
-            link: "/pages/b54954/",
-          },
-          {
-            text: "Docker 命令大全",
-            link: "/pages/086726/",
-          },
-        ]
-      },
-      {
-        text: "服务",
-        items: [
-          {
-            text: "Tomcat",
-            link: "/pages/9785e2/",
-          },
-          {
-            text: "Nginx",
-            link: "/pages/df00c4/",
-          },
-          {
-            text: "Kafka",
-            link: "/pages/ae56b2/",
-          },
-        ]
-      },
-      {
-        text: "其他",
-        items: [
-          {
-            text: "内网穿透",
-            link: "/pages/5e0765/",
-          },
-        ]
-      },
-    ]
-  },
-
-  {
-    text: "前端",
-    items: [
-      {
-        text: "前端框架",
+        text: "前端",
         items: [
           {
             text: "Vue.js",
@@ -189,36 +37,66 @@ module.exports = [
           },
         ]
       },
-    ]
-  },
-  {
-    text: "测试",
-    items: [
       {
-        text: "测试基础",
-        link: "/pages/0f55d1/",
-      },
-      {
-        text: "测试框架",
-        link: "/pages/be1737/",
-      }
-    ]
-  },
-  {
-    text: "Python",
-    items: [
-      {
-        text: "Python ui自动化测试",
+        text: "测试",
         items: [
           {
-            text: "Python ui自动化测试",
-            link: "/pages/8e98a2/",
+            text: "测试基础",
+            link: "/pages/0f55d1/",
           },
           {
-            text: "Python scrapy爬虫",
-            link: "/pages/7fdf13/",
+            text: "测试框架",
+            link: "/pages/be1737/",
+          }
+        ]
+      },
+      {
+        text: "Python",
+        items: [
+          {
+            text: "Python 使用",
+            link: "/pages/8e98a2/",
           },
         ]
+      },
+    ]
+  },
+  {
+    text: "运维",
+    items: [
+      {
+        text: "数据库",
+        items: [
+          {
+            text: "Mysql",
+            link: "/pages/d51cca/",
+          },
+          {
+            text: "MongoDB",
+            link: "/pages/b58ac5/",
+          },
+          {
+            text: "Redis",
+            link: "/pages/3ae5e3/",
+          },
+        ],
+      },
+      {
+        text: "部署",
+        items: [
+          {
+            text: "服务器",
+            link: "/pages/fc21a3/",
+          },
+          {
+            text: "Docker",
+            link: "/pages/b54954/",
+          },
+          {
+            text: "服务",
+            link: "/pages/9785e2/",
+          },
+        ],
       },
     ]
   },
